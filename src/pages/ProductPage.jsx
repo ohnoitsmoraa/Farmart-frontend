da814612967457e9961c36a2d0a442042289db52
Kiprotichabiud <abiud.kiprotich@student.moringaschool.com>
2024-11-15 17:27:45 +0300

    updated productpage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,26 +3,26 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const animalsData = [
-    { id: 1, name: 'Cow', image: 'https://media.istockphoto.com/id/2128077473/photo/jersey-cow-side-view-in-a-green-pasture-looking-and-large-udder.jpg?s=612x612&w=0&k=20&c=yCHRiatMo4nouALd1uc6GUsuhS3_rCtpgMs6T9fQK3g=', price: 20000, age: 5, breed: 'Jersey' },
-    { id: 2, name: 'Sheep', image: 'https://media.istockphoto.com/id/165847420/photo/sheep.jpg?s=612x612&w=0&k=20&c=-6FtvuuBnpo7RiP8Y_LR4yHG6-yuOaWnwu4uDuhdg94=', price: 7000, age: 3, breed: 'Merino' },
-    { id: 3, name: 'Pig', image: 'https://media.istockphoto.com/id/869822406/photo/beautiful-young-female-sow-grazing-on-pasture-summertime.jpg?s=612x612&w=0&k=20&c=VuEED7H_3c0CcH9Hrx6PeHrUenztqI8ZoUuH_ZlPJqY=', price: 10000, age: 2, breed: 'Yorkshire' },
-    { id: 4, name: 'Goat', image: 'https://media.istockphoto.com/id/1495083421/photo/boer-goat-walking-in-a-green-grassy-field.jpg?s=612x612&w=0&k=20&c=3qQgA2uUetmZ_guz5nm1jThReHb8oqbdu9KIjJqXppY=', price: 8000, age: 4, breed: 'Boer' },
-    { id: 5, name: 'Horse', image: 'https://media.istockphoto.com/id/1427594977/photo/arabian-horse-beautiful-arabian-horse.jpg?s=612x612&w=0&k=20&c=26NsTNFKa7thTlXzVEtsN3O-E1ln0jQcTTp2LJuoaAw=', price: 25000, age: 6, breed: 'Arabian' },
-    { id: 6, name: 'Cat', image: 'https://media.istockphoto.com/id/176697468/photo/view-siamese-cat.jpg?s=612x612&w=0&k=20&c=DpbQPqJAZKvwFPr8_iQm1CvNQ_6UeIlzHKqL3wsx3aA=', price: 2000, age: 1, breed: 'Siamese' },
-    { id: 7, name: 'Rabbit', image: 'https://media.istockphoto.com/id/1309323293/photo/a-furry-english-angora-rabbit.jpg?s=612x612&w=0&k=20&c=n1BQ-IuC9rWAGPp0Z2NVxVk954xPURJ1WQcy077Pjyw=', price: 1500, age: 1, breed: 'Angora' },
-    { id: 8, name: 'Donkey', image: 'https://media.istockphoto.com/id/2133540563/photo/donkey-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=n0ng7EwHYNxti6yZzcFdXCbERrzzogKYqcmnl70IQiQ=', price: 12000, age: 7, breed: 'Miniature' },
-    { id: 9, name: 'Llama', image: 'https://media.istockphoto.com/id/139880793/photo/alpaca.jpg?s=612x612&w=0&k=20&c=XLWF32g2AMszGscTZhNbIuxAnTJqomETHmSPeMTHjSE=', price: 18000, age: 5, breed: 'Suri' },
-    { id: 10, name: 'Duck', image: 'https://media.istockphoto.com/id/2159166035/photo/american-pekin-duck.jpg?s=612x612&w=0&k=20&c=sZHV_y_xFft29dgfuFBeoK8atnzjx4L2L5f-QewB6oA=', price: 500, age: 1, breed: 'Pekin' },
-    { id: 11, name: 'Chicken', image: 'https://media.istockphoto.com/id/1746877640/photo/white-hen-isolated.jpg?s=612x612&w=0&k=20&c=P3p0vVw4UNlU4B_bdmof6-F_cUJ2mVHWVoMls0shdE0=', price: 300, age: 1, breed: 'Leghorn' },
-    { id: 12, name: 'Alpaca', image: 'https://media.istockphoto.com/id/1453150611/photo/brown-white-calico-alpaca-llama.jpg?s=612x612&w=0&k=20&c=1RkSWBYvFbnuqqtdVwdTWNjJQ6Hdf-QvH4R8_drm3cQ=', price: 17000, age: 4, breed: 'Huacaya' },
-    { id: 13, name: 'Turkey', image: 'https://media.istockphoto.com/id/1285143579/photo/turkey-cock-with-spread-wings-isolated-on-white.jpg?s=612x612&w=0&k=20&c=yZv-8et5DAMQpNePH470OMCalyHVn2BZ3R3Tebv1rvc=', price: 800, age: 2, breed: 'Bronze' },
-    { id: 14, name: 'Dog', image: 'https://media.istockphoto.com/id/186869569/photo/labrador-dog-sitting-on-white-background.jpg?s=612x612&w=0&k=20&c=cIlHzT0f6KtFZSk8Bp8ilicyJJRXSgUwV4OcM-IsIUk=', price: 5000, age: 3, breed: 'Labrador' },
-    { id: 15, name: 'Cat', image: 'https://media.istockphoto.com/id/182781330/photo/playful-cat.jpg?s=612x612&w=0&k=20&c=fOO_nv5zjNa90t1PXONptHE0dTS-m55f-5gmZ49dioE=', price: 2500, age: 2, breed: 'Bengal' },
-    { id: 16, name: 'Pigeon', image: 'https://media.istockphoto.com/id/824175620/photo/homing-pigeon-pooping-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=rAo4EK70V4Bc7fba0yaWl3kHkERfTGvcbHlIalRncT0=', price: 200, age: 1, breed: 'Homing' },
-    { id: 17, name: 'Camel', image: 'https://media.istockphoto.com/id/1190556631/photo/camel-isolated-on-the-white-background.jpg?s=612x612&w=0&k=20&c=4ig76eF0CzsD7kloGPJ6kz24ctHZQoTj81-oyyuuMLg=', price: 30000, age: 8, breed: 'Dromedary' },
-    { id: 18, name: 'Goose', image: 'https://media.istockphoto.com/id/537545716/photo/chinese-dumb-duck-isolated-over-white-background.jpg?s=612x612&w=0&k=20&c=77bWjsEEOacnGo6Wb1xNUS6xNn7e4_iGHaR0tceNw0Q=', price: 700, age: 2, breed: 'Embden' },
-    { id: 19, name: 'Ferret', image: 'https://media.istockphoto.com/id/922768006/photo/ferret-baby-on-white-background.jpg?s=612x612&w=0&k=20&c=00PqYPqW5bvBFqiSkRgjMMv00b4NI1zrq5Cg5bi10vo=', price: 1200, age: 1, breed: 'Sable' },
-    { id: 20, name: 'Fish', image: 'https://media.istockphoto.com/id/1205505358/photo/goldfish-isolated-on-a-dark-black-background.jpg?s=612x612&w=0&k=20&c=5SstzozoVakhyJeKdgLZNmG9rsz9M6-ngUUp3Zx411U=', price: 50, age: 1, breed: 'Goldfish' },
+  { id: 1, name: 'Cow', image: 'https://media.istockphoto.com/id/535377969/photo/brown-cow.jpg?s=612x612&w=0&k=20&c=_s24FrK61f5A1OIVEZvvTC80zQeh8Tlz7PvfjwZRWTs=', price: 20000, age: 5, breed: 'Jersey', status: 'Available' },
+  { id: 2, name: 'Sheep', image: 'https://media.istockphoto.com/id/102716830/photo/front-view-of-arles-merino-sheep-ram-standing.jpg?s=612x612&w=0&k=20&c=ncY5w-TPiuvzGS7TDaubFu1xQlFtL5gnKTYojOQRIdk=', price: 7000, age: 3, breed: 'Merino', status: 'Sold' },
+  { id: 3, name: 'Pig', image: 'https://media.istockphoto.com/id/1139789449/photo/young-pig-on-white-background.jpg?s=612x612&w=0&k=20&c=jep4wpqBabfoeKvQtra0vOnT61darFuzR6Kxip19MeE=', price: 10000, age: 2, breed: 'Yorkshire', status: 'Available' },
+  { id: 4, name: 'Goat', image: 'https://media.istockphoto.com/id/816545134/photo/boer-goat-on-white.jpg?s=612x612&w=0&k=20&c=HUz68tV5icKqus8fUWNPQEd9CSb9yQ7VxPLgcdYws7o=', price: 8000, age: 4, breed: 'Boer', status: 'Sold' },
+  { id: 5, name: 'Horse', image: 'https://media.istockphoto.com/id/457769421/photo/arabian-horse-mare-isolated-on-white.jpg?s=612x612&w=0&k=20&c=uuOrM9v0PcECy4SOemJhyN2D3w9hajXVTVpVzsrr7tk=', price: 25000, age: 6, breed: 'Arabian', status: 'Available' },
+  { id: 6, name: 'Cat', image: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=', price: 2000, age: 1, breed: 'Siamese', status: 'Available' },
+  { id: 7, name: 'Rabbit', image: 'https://media.istockphoto.com/id/450608541/photo/rabbit-sitting-on-white-background.jpg?s=612x612&w=0&k=20&c=0yaStTEqOXrbpkEHeUQ7n_QSNtSXJ1tgkLZdK_bpMY0=', price: 1500, age: 1, breed: 'Angora', status: 'Sold' },
+  { id: 8, name: 'Donkey', image: 'https://media.istockphoto.com/id/2133540563/photo/donkey-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=n0ng7EwHYNxti6yZzcFdXCbERrzzogKYqcmnl70IQiQ=', price: 12000, age: 7, breed: 'Miniature', status: 'Available' },
+  { id: 9, name: 'Sheep', image: 'https://media.istockphoto.com/id/665135234/photo/sheep-farm-animal-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=TsO0kDhPS4D2N_bfBgXzFEMmnLp9QP0CI0lP3mA_9dA=', price: 8000, age: 5, breed: 'Dorper', status: 'Available' },
+  { id: 10, name: 'Duck', image: 'https://media.istockphoto.com/id/2159166035/photo/american-pekin-duck.jpg?s=612x612&w=0&k=20&c=sZHV_y_xFft29dgfuFBeoK8atnzjx4L2L5f-QewB6oA=', price: 500, age: 1, breed: 'Pekin', status: 'Sold' },
+  { id: 11, name: 'Chicken', image: 'https://media.istockphoto.com/id/1146111649/photo/brown-dwarf-hen-on-white-background.jpg?s=612x612&w=0&k=20&c=JtTYHwDfZAdRiUMHoHohkLXpeSA1agiUp93xGwdFWtA=', price: 300, age: 1, breed: 'Leghorn', status: 'Available' },
+  { id: 12, name: 'Alpaca', image: 'https://media.istockphoto.com/id/1453150611/photo/brown-white-calico-alpaca-llama.jpg?s=612x612&w=0&k=20&c=1RkSWBYvFbnuqqtdVwdTWNjJQ6Hdf-QvH4R8_drm3cQ=', price: 17000, age: 4, breed: 'Huacaya', status: 'Sold' },
+  { id: 13, name: 'Turkey', image: 'https://media.istockphoto.com/id/1285143579/photo/turkey-cock-with-spread-wings-isolated-on-white.jpg?s=612x612&w=0&k=20&c=yZv-8et5DAMQpNePH470OMCalyHVn2BZ3R3Tebv1rvc=', price: 800, age: 2, breed: 'Bronze', status: 'Available' },
+  { id: 14, name: 'Dog', image: 'https://media.istockphoto.com/id/1283671216/photo/attentive-black-labrador-dog-looking-up-side-view-isolated-on-white-background-obedience.jpg?s=612x612&w=0&k=20&c=91gaRzAW5SR-0IkLHxzSQu5gwNFVnqgLgs7F6CdSq2E=', price: 5000, age: 3, breed: 'Labrador', status: 'Available' },
+  { id: 15, name: 'Cat', image: 'https://media.istockphoto.com/id/182781330/photo/playful-cat.jpg?s=612x612&w=0&k=20&c=fOO_nv5zjNa90t1PXONptHE0dTS-m55f-5gmZ49dioE=', price: 2500, age: 2, breed: 'Bengal', status: 'Sold' },
+  { id: 16, name: 'Pigeon', image: 'https://media.istockphoto.com/id/824175620/photo/homing-pigeon-pooping-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=rAo4EK70V4Bc7fba0yaWl3kHkERfTGvcbHlIalRncT0=', price: 200, age: 1, breed: 'Homing', status: 'Available' },
+  { id: 17, name: 'Camel', image: 'https://media.istockphoto.com/id/1190556631/photo/camel-isolated-on-the-white-background.jpg?s=612x612&w=0&k=20&c=4ig76eF0CzsD7kloGPJ6kz24ctHZQoTj81-oyyuuMLg=', price: 30000, age: 8, breed: 'Dromedary', status: 'Available' },
+  { id: 18, name: 'Goose', image: 'https://media.istockphoto.com/id/537545716/photo/chinese-dumb-duck-isolated-over-white-background.jpg?s=612x612&w=0&k=20&c=77bWjsEEOacnGo6Wb1xNUS6xNn7e4_iGHaR0tceNw0Q=', price: 700, age: 2, breed: 'Embden', status: 'Sold' },
+  { id: 19, name: 'Cow', image: 'https://media.istockphoto.com/id/505137898/photo/holstein-cow.jpg?s=612x612&w=0&k=20&c=Jkgj5lzrC-B9pdlfalX3k1UjHmgV-cJsI1F17QFsIjc=', price: 50000, age: 4, breed: 'Fresian', status: 'Available' },
+  { id: 20, name: 'Dog', image: 'https://media.istockphoto.com/id/146889930/photo/german-shepherd-isolated-on-white.jpg?s=612x612&w=0&k=20&c=SqkEVxHGRuHdW1faF8WIswsVs04QanAmausbNUaVE6Y=', price: 45000, age: 2, breed: 'German Shepherd', status: 'Available' }
 ];
 
 const ProductPage = ({ addToCart, cartItems }) => {
@@ -43,7 +43,7 @@ const ProductPage = ({ addToCart, cartItems }) => {
         <div className="flex items-center space-x-4">
           <input
             type="text"
-            placeholder="Search animals by name..."
+            placeholder="Search..."
             className="p-2 border border-gray-300 rounded-md"
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -61,20 +61,25 @@ const ProductPage = ({ addToCart, cartItems }) => {
       </div>
 
       {/* Animal Listings */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
         {filteredAnimals.slice(0, displayLimit).map((animal) => (
-          <div key={animal.id} className="border rounded-lg p-4 bg-white shadow-md">
-            <img src={animal.image} alt={animal.name} className="h-64 w-full object-cover mb-2 rounded-md" />
+          <div key={animal.id} className="border rounded-lg p-6 bg-white shadow-lg shadow-gray-400 hover:shadow-2xl transform transition-shadow duration-300">
+            <img src={animal.image} alt={animal.name} className="h-56 w-full object-cover mb-2 rounded-md" />
             <h3 className="text-xl font-semibold">{animal.name}</h3>
             <p className="text-gray-700">Breed: {animal.breed}</p>
             <p className="text-gray-700">Age: {animal.age}</p>
             <p className="text-green-800 font-bold">Kes {animal.price}</p>
-            <button
-              onClick={() => addToCart(animal)}
-              className="mt-2 bg-green-900 text-white px-4 py-2 rounded-md hover:bg-green-700"
-            >
-              Add to cart
-            </button>
+            <p className={`font-bold ${animal.status === 'Available' ? 'text-green-600' : 'text-red-600'}`}>
+              {animal.status}
+            </p>
+            {animal.status === 'Available' && (
+              <button
+                onClick={() => addToCart(animal)}
+                className="mt-4 bg-green-900 text-white px-4 py-2 rounded-md hover:bg-green-700"
+              >
+                Add to cart
+              </button>
+            )}
           </div>
         ))}
       </div>
@@ -83,7 +88,7 @@ const ProductPage = ({ addToCart, cartItems }) => {
       {filteredAnimals.length > displayLimit && (
         <button
           onClick={() => setDisplayLimit(displayLimit + 12)}
-          className="mt-4 p-10  bg-green-900 text-white py-2 rounded-md hover:bg-green-600"
+          className="mt-4 p-10 bg-green-900 text-white py-2 rounded-md hover:bg-green-600"
         >
           See All
         </button>
