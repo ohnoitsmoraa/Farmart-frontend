102416a431a6bb8e27b9b1f36ce9205f3009ea8f
Kiprotichabiud <abiud.kiprotich@student.moringaschool.com>
2024-11-15 18:28:09 +0300

    added more stylings

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,20 +15,20 @@ const Login = () => {
         
         {/* Right side with form */}
         <div className="w-full md:w-1/2 p-8">
-          <button className="text-gray-600 text-sm mb-4">Back</button>
+         
           <h2 className="text-3xl font-bold text-gray-800 mb-2">Login to your account</h2>
-          <p className="text-gray-500 mb-6">Don't have an account? <a href="#" className="text-green-600 hover:underline">Sign up</a></p>
+          <p className="text-gray-500 mb-6">Don't have an account? <a href="#" className="text-green-600 hover:underline">Register</a></p>
           
           {/* Toggle for Farmer/Buyer */}
-          <div className="flex items-center mb-6">
+          <div className="flex items-center mb-6 justify-center">
             <button
-              className={`w-1/2 py-2 text-center font-semibold ${isFarmer ? 'bg-green-700 text-white' : 'bg-gray-300 text-gray-700'}`}
+              className={` px-6 py-2 text-center font-semibold ${isFarmer ? 'bg-green-700 text-white' : 'bg-gray-300 text-gray-700'}`}
               onClick={() => setIsFarmer(true)}
             >
               Farmer
             </button>
             <button
-              className={`w-1/2 py-2 text-center font-semibold ${!isFarmer ? 'bg-green-700 text-white' : 'bg-gray-300 text-gray-700'}`}
+              className={` px-6 py-2 text-center font-semibold ${!isFarmer ? 'bg-green-700 text-white' : 'bg-gray-300 text-gray-700'}`}
               onClick={() => setIsFarmer(false)}
             >
               Buyer
